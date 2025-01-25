2655cbea82aeea37bfed21510ef50640dc0abc44
Laptop Mauricio <dmtg2212@gmail.com>
2025-01-25 14:12:55 -0600

    Create Ejercicios.py (Comments)

---
+++ Semana 1/Ejercicios.py
@@ -0,0 +1,63 @@
+# Pregunta 1: ¿Cuál es la función de la memoria secundaria
+#               de la computadora?
+#   Respuesta: 
+#   c) Almacenar información durante mucho tiempo, incluso
+#   después de ciclos de apagado y encendido.
+
+# Pregunta 2: ¿Qué es un programa?
+#   Respuesta:
+#   Es una secuencia o un conjunto de instrucciones que
+#   llevan acabo un proceso.
+
+# Pregunta 3: ¿Cuál es la diferencia entre un compilador y
+#               un interprete?
+#   Respuesta:
+#   El compilador traduce de codigo fuente a lenguaje de maquina
+#   y genera un archivo listo para ser ejecutado, mientras que el
+#   intérprete traduce y ejecuta el programa en tiempo real, la diferencia
+#   es que el compilador genera un archivo listo para ejecutarse de manera
+#   rapida y traducida a lenguaje de maquina, y el intérprete va traduciendo
+#   el código fuente en tiempo real.
+
+# Pregunta 4: ¿Cuál de los siguientes contiene "código máquina"?
+#   Respuesta:
+#   a) El intérprete de Python
+
+# Pregunta 5: ¿Qué está mal en el siguiente código?
+#   Respuesta:
+#   La instrucción 'primt()' es la incorrecta, la forma
+#   correcta de escribir la instrucción es 'print()'
+
+# Pregunta 6: ¿En qué lugar del computador queda almacenada una variable
+#               después de ejectuar el programa?
+#   Respuesta:
+#   b) En la memoria principal.
+
+# Pregunta 7: ¿Qué mostrará en pantalla el siguiente programa?
+#           x = 43
+#           x = x + 1
+#           print(x)
+#   Respuesta:
+#   b) 44
+# Pregunta 8: Explica los conceptos usando un ejemplo de una capacidad humana
+#   Respuesta:
+#   1. Unidad Central de Procesamiento: el CPU sería como el cerebro humano,
+#   procesa toda la información y manda a ejecutar todas las instrucciones al
+#   resto del cuerpo.
+#   2. Memoria principal: sería como la memoria a corto plazo de una persona,
+#   se guarda información de manera temporal.
+#   3. Memoria secundaria: sería como la memoria a largo plazo, la información
+#   estará ahí de manera permanente, también podría ser como una carpeta externa
+#   a la que podríamos acceder cuando nosotros querramos.
+#   4. Dispositivos de entrada: sería como si una persona te estuviera hablando
+#   para contarte algo, te esta dando información para que tu la proceses.
+#   5. Dispositivos de salidad: con la misma analogía de los dispositivos de entrada,
+#   los dispositivos de salida sería tú respondiendo después de haber escuchado la
+#   historia recién contada.
+
+# Pregunta 9: ¿Cómo puedes corregir un "Error de sintaxis"?
+#   Respuesta:
+#   Hay varias formas de corregir un error de sintaxis, primero podrías revisar la
+#   terminal generalmente la máquina te da "pistas" de dónde podría estar tú error,
+#   también puedes volver a leer el código minuciosamente para intentar encontrar tu error,
+#   y por último revisar la gramática del código fuente para corregir tu error.
